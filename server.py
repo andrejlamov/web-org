1428faf31d747e41609e24557c6739c96075b2fd
Andrej Lamov <andrej.lamov@gmail.com>
2016-02-21 14:58:40 -0500

    Make communication bidirectional
    Poll file every 2 seconds, if change emit to all.

--- server.py
+++ server.py
@@ -1,13 +1,28 @@
 from flask import Flask, json, render_template
 from flask_socketio import SocketIO, emit
-
+import threading
 import pyorg
+import os
 import time
-import datetime as dt
+import eventlet
+
+eventlet.monkey_patch()
 
 app = Flask(__name__);
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+
+last_changed = time.time()
+def watch():
+    global last_changed
+    while True:
+        time.sleep(2)
+        new_changed = os.stat('example.org').st_mtime
+        if new_changed > last_changed:
+            print 'org file on server updated'
+            socketio.emit("data", pyorg.parseorg('example.org'))
+            last_changed = new_changed
 
 
 @app.route("/")
@@ -24,10 +39,13 @@ def data(data):
     return
 
 @socketio.on('bonjour')
-def bonjour(org):
-    return
+def bonjour(msg):
+    print 'Client connected'
 
 if __name__ == "__main__":
+    thread = threading.Thread(target = watch)
+    thread.daemon = True
+    thread.start()
     app.debug = True
     socketio.run(app, host='0.0.0.0', port=1337)
 
